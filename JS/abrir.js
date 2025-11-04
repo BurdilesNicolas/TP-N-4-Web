7e91374fe924bbe7df2668786f4888fca73d8a1e
BurdilesNicolas <burdilescarroandres.epet20@gmail.com>
2025-11-04 15:25:17 -0300

    Modificando detalles en varios archivos

--- JS/abrir.js
+++ JS/abrir.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", function (){
-    const botonCuenta = document.getElementById("cuenta"); // Establece la existencia del botón 
+//Script para abrir formulario de cuenta
+document.addEventListener("DOMContentLoaded", function (){ // Espera a que carge todo el documento
+    const botonCuenta = document.getElementById("cuenta");
     
     if (botonCuenta) {
-        botonCuenta.addEventListener("click", function (){
+        botonCuenta.addEventListener("click", function (){ // Escucha el evento de click sobre el botón
             window.location.href = "cuenta.html"; // Te dirige al formulario de inicio de sesión de cuenta.html
         });
     }
